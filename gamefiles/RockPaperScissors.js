b3ffb2ef071356decaef5a4429c78000c9e8c205
Jimmy Tran <jtran549@yahoo.com>
2020-08-01 17:41:26 -0400

    Added basic rock paper scissors logic

---
+++ gamefiles/RockPaperScissors.js
@@ -0,0 +1,47 @@
+class RockPaperScissors {
+    constructor() {
+        this.userScore = 0;
+        this.cpuScore = 0;
+        this.round = 0;
+    }
+
+    generateCPUResponse() {
+        const acceptedValues = [`rock`, `paper`, `scissors`];
+
+        return acceptedValues[Math.floor(Math.random() * (3)) + 0];
+    }
+
+    determineWinner(userSelection, cpuSelection) {
+        if (userSelection == cpuSelection) {
+            return 'tie';
+        }
+        else if ((userSelection == "rock" && cpuSelection == "scissors") || (userSelection == "paper" && cpuSelection == "rock") || (userSelection == "scissors" && cpuSelection == "paper")) {
+            return 'win'
+        }
+        else {
+            return 'lose';
+        }
+    }
+
+    play(userSelection) {
+        var cpuSelection = this.generateCPUResponse();
+        var result = this.determineWinner(userSelection, cpuSelection);
+        if (result == 'win') {
+            this.userScore++;
+            this.round++
+        }
+        else if (result == 'lose') {
+            this.cpuScore++;
+            this.round++;
+        }
+        if (this.round == 5) {
+            if(this.userScore > this.cpuScore){
+                return "playerWin";
+            }
+            else if(this.userScore < this.cpuScore){
+                return "cpuWin";
+            }
+        }
+    }
+}
+
